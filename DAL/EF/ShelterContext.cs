56343765d28d6af9963fdede10c96264d3ab648a
Yukont <94931609+Yukont@users.noreply.github.com>
2023-11-15 01:32:56 +0300

    update context

--- DAL/EF/ShelterContext.cs
+++ DAL/EF/ShelterContext.cs
@@ -16,6 +16,8 @@ public partial class ShelterContext : DbContext
     {
     }
 
+    public string ConnectionString { get; }
+
     public virtual DbSet<AdoptionApplication> AdoptionApplications { get; set; }
 
     public virtual DbSet<AdoptionStatus> AdoptionStatuses { get; set; }
@@ -59,8 +61,7 @@ public partial class ShelterContext : DbContext
     public virtual DbSet<Veterinarian> Veterinarians { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=.;Database=shelter;Trusted_Connection=True;TrustServerCertificate=true");
+        => optionsBuilder.UseSqlServer(ConnectionString);
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
