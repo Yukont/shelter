55ca105bb6929bab6345ec622b8df56d101f8c1b
Yukont <94931609+Yukont@users.noreply.github.com>
2023-11-15 03:25:54 +0300

    add UoW

---
+++ DAL/EFUnitOfWork/EFUnitOfWork.cs
@@ -0,0 +1,106 @@
+﻿using DAL.EF;
+using DAL.Interfaces;
+using DAL.Repositories;
+using Microsoft.Extensions.Configuration;
+using shelter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.EFUnitOfWork
+{
+    internal class EFUnitOfWork : IUnitOfWork
+    {
+        private ShelterContext db;
+        private AdoptionApplicationRepository adoptionApplicationRepository;
+        private AdoptionStatusRepository adoptionStatusRepository;
+        private AnimalRepository animalRepository;
+        private AnimalStatusRepository animalStatusRepository;
+        private AppointmentRepository appointmentRepository;
+        private ClinicRepository clinicRepository;
+        private DonationRepository donationRepository;
+        private EventScheduleRepository eventScheduleRepository;
+        private GenderRepository genderRepository;
+        private PositionRepository positionRepository;
+        private ReviewRepository reviewRepository;
+        private ShelterInformationRepository shelterInformationRepository;
+        private SpeciallizationRepository speciallizationRepository;
+        private SpeciesRepository speciesRepository;
+        private StaffRepository staffRepository;
+        private StaffRoleRepository staffRoleRepository;
+        private StatusOfHealthRepository statusOfHealthRepository;
+        private UserRepository userRepository;
+        private UserRoleRepository userRoleRepository;
+        private UsersGenderRepository usersGenderRepository;
+        private VeterinarianRepository veterinarianRepository;
+
+        public EFUnitOfWork(IConfiguration configuration) 
+        {
+            string connectionString = configuration.GetConnectionString("DefaultConnection");
+            db = new ShelterContext(connectionString);
+        }
+
+        public IRepository<AdoptionApplicationRepository> AdoptionApplication => throw new NotImplementedException();
+
+        public IRepository<AdoptionStatusRepository> AdoptionStatus => throw new NotImplementedException();
+
+        public IRepository<Animal> Animals
+        {
+            get
+            {
+                if (animalRepository == null)
+                    animalRepository = new AnimalRepository(db);
+                return animalRepository;
+            }
+        }
+        public IRepository<AnimalRepository> Animal => AnimalRepository ??= new AnimalRepository(db);
+
+        public IRepository<AnimalStatusRepository> AnimalStatus => throw new NotImplementedException();
+
+        public IRepository<AppointmentRepository> Appointment => throw new NotImplementedException();
+
+        public IRepository<ClinicRepository> Clinic => throw new NotImplementedException();
+
+        public IRepository<DonationRepository> Donation => throw new NotImplementedException();
+
+        public IRepository<EventScheduleRepository> EventSchedule => throw new NotImplementedException();
+
+        public IRepository<GenderRepository> Gender => throw new NotImplementedException();
+
+        public IRepository<PositionRepository> Position => throw new NotImplementedException();
+
+        public IRepository<ReviewRepository> Review => throw new NotImplementedException();
+
+        public IRepository<ShelterInformationRepository> ShelterInformation => throw new NotImplementedException();
+
+        public IRepository<SpeciallizationRepository> Speciallization => throw new NotImplementedException();
+
+        public IRepository<SpeciesRepository> Species => throw new NotImplementedException();
+
+        public IRepository<StaffRepository> Staff => throw new NotImplementedException();
+
+        public IRepository<StaffRoleRepository> StaffRole => throw new NotImplementedException();
+
+        public IRepository<StatusOfHealthRepository> StatusOfHealth => throw new NotImplementedException();
+
+        public IRepository<UserRepository> User => throw new NotImplementedException();
+
+        public IRepository<UserRoleRepository> UserRole => throw new NotImplementedException();
+
+        public IRepository<UsersGenderRepository> UsersGender => throw new NotImplementedException();
+
+        public IRepository<VeterinarianRepository> Veterinarian => throw new NotImplementedException();
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DAL/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,35 @@
+﻿using DAL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+    internal interface IUnitOfWork : IDisposable
+    {
+        IRepository<AdoptionApplicationRepository> AdoptionApplication { get; }
+        IRepository<AdoptionStatusRepository> AdoptionStatus { get; }
+        IRepository<AnimalRepository> Animal { get; }
+        IRepository<AnimalStatusRepository> AnimalStatus { get; }
+        IRepository<AppointmentRepository> Appointment { get; }
+        IRepository<ClinicRepository> Clinic { get; }
+        IRepository<DonationRepository> Donation { get; }
+        IRepository<EventScheduleRepository> EventSchedule{ get; }
+        IRepository<GenderRepository> Gender { get; }
+        IRepository<PositionRepository> Position { get; }
+        IRepository<ReviewRepository> Review { get; }
+        IRepository<ShelterInformationRepository> ShelterInformation { get; }
+        IRepository<SpeciallizationRepository> Speciallization { get; }
+        IRepository<SpeciesRepository> Species { get; }
+        IRepository<StaffRepository> Staff { get; }
+        IRepository<StaffRoleRepository> StaffRole { get; }
+        IRepository<StatusOfHealthRepository> StatusOfHealth { get; }
+        IRepository<UserRepository> User { get; }
+        IRepository<UserRoleRepository> UserRole { get; }
+        IRepository<UsersGenderRepository> UsersGender { get; }
+        IRepository<VeterinarianRepository> Veterinarian { get; }
+        void Save();
+    }
+}
