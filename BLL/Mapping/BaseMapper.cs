75e4d631deb0e9ba15ee184b86829548039dc725
Yukont <94931609+Yukont@users.noreply.github.com>
2023-11-21 05:31:14 +0300

    add mapping

---
+++ BLL/Mapping/BaseMapper.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Mapping
+{
+    internal class BaseMapper<TSource, TDestination> : Profile
+    {
+        public BaseMapper()
+        {
+            CreateMap<TSource, TDestination>();
+            CreateMap<TDestination, TSource>();
+        }
+    }
+}
