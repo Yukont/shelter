b055d1ec4f682a7576e76707ca27c4ddb8a409e8
Yukont <94931609+Yukont@users.noreply.github.com>
2023-11-28 15:18:53 +0300

    Add interfaces

---
+++ BLL/Interfaces/IAdoptionApplicationService.cs
@@ -0,0 +1,19 @@
+﻿using BLL.DTO;
+using shelter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Interfaces
+{
+    internal interface IAdoptionApplicationService : IDisposable
+    {
+        Task AddAdoptionApplication(AdoptionApplicationDTO adoptionApplicationDto);
+        Task UpdateAdoptionApplication(AdoptionApplicationDTO adoptionApplicationDto);
+        Task RemoveAdoptionApplication(int adoptionApplicationId);
+        Task<AdoptionApplicationDTO> GetAdoptionApplicationById(int adoptionApplicationId);
+        Task<IEnumerable<AdoptionApplicationDTO>> GetAllAdoptionApplications();
+    }
+}
---
+++ BLL/Interfaces/IAdoptionStatusService.cs
@@ -0,0 +1,18 @@
+﻿using BLL.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Interfaces
+{
+    public interface IAdoptionStatusService : IDisposable
+    {
+        Task AddAdoptionStatus(AdoptionStatusDTO adoptionStatusDto);
+        Task UpdateAdoptionStatus(AdoptionStatusDTO adoptionStatusDto);
+        Task RemoveAdoptionStatus(int adoptionStatusId);
+        Task<AdoptionStatusDTO> GetAdoptionStatusById(int adoptionStatusId);
+        Task<IEnumerable<AdoptionStatusDTO>> GetAllAdoptionStatus();
+    }
+}
