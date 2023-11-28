bd88d2b5d26ffa289a94ce25454d047f17bde199
Yukont <94931609+Yukont@users.noreply.github.com>
2023-11-28 15:17:19 +0300

    add mapping

---
+++ shelter/Mapping/AdoptionStatusMapper.cs
@@ -0,0 +1,15 @@
+﻿using BLL.DTO;
+using shelter;
+using shelter.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shelter.Mapping
+{
+    internal class AdoptionStatusMapper : BaseMapper<AdoptionStatusDTO, AdoptionStatusViewModel>
+    {
+    }
+}
---
+++ shelter/Mapping/BaseMapper.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shelter.Mapping
+{
+    internal class BaseMapper<TSource, TDestination> : Profile
+    {
+        public BaseMapper()
+        {
+            CreateMap<TSource, TDestination>();
+            CreateMap<TDestination, TSource>();
+        }
+    }
+}
